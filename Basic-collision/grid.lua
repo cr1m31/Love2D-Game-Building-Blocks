400d2fc1dd25a1570fbc5149d7384a3debdc3c04
Claude Rime <claude.rime@marvelous.digital>
2025-06-04 15:13:49 +0200

    saved tile locations in table then draw tiles table

--- Basic-collision/grid.lua
+++ Basic-collision/grid.lua
@@ -17,35 +17,32 @@ local cell = {
     height = 50,
 }
 
-function gridModule.createGridTiles()
-  local tileNum = 0
-  for rowNumber, row in ipairs(grid) do
-    for columnNumber, tileValue in ipairs(grid[rowNumber]) do
-      tileNum = tileNum + 1
-      if tileValue == 1 then
-        print("tileNum: " .. tileNum)
-        print("rowNumber: " .. rowNumber .. "\ncell y: " .. rowNumber * cell.height)
-        print("columnNumber: " .. columnNumber .. "\ncell x: " .. columnNumber * cell.width)
+local buildedTiles = {}
+
+function buildGridTileLocations()
+  tiles = {}
+  for rowNum, row in ipairs(grid) do
+    for columnNumber, tileValue in ipairs(grid[rowNum]) do
+      if tileValue == 1 then -- add only tiles with value 1 of the grid
+        table.insert(tiles, {
+          x = (columnNumber -1) * cell.width,
+          y = (rowNum -1) * cell.height,
+        })
       end
     end
   end
+  return tiles
 end
 
-function gridModule.drawGrid()
-  for rowNumber, row in ipairs(grid) do
-    for columnNumber, tileValue in ipairs(grid[rowNumber]) do
-      if tileValue == 0 then
-        -- love.graphics.setColor(1,0,0)
-        love.graphics.rectangle("line", cell.width * (columnNumber - 1), cell.height * (rowNumber - 1), cell.width, cell.height)
-      end
-      
-      if tileValue == 1 then
-        love.graphics.setColor(1,1,1)
-        love.graphics.rectangle("fill", cell.width * (columnNumber - 1), cell.height * (rowNumber - 1), cell.width, cell.height)
-      end
-    end
+function gridModule.loadBuildedTiles()
+  buildedTiles = buildGridTileLocations()
+end
+
+function gridModule.drawTilesOnGrid()
+  for tileNum, tile in ipairs(buildedTiles) do
+    love.graphics.setColor(1,1,1)
+    love.graphics.rectangle("line", tile.x, tile.y, cell.width, cell.height)
   end
 end
-
 
 return gridModule
