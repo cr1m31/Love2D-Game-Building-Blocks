4536269880c8ea20c7f0c472ea61ae775740cf39
Claude Rime <claude.rime@marvelous.digital>
2025-06-11 10:24:46 +0200

    added basic collision with velocity folder in master

---
+++ Basic-collision-with-velocity/player.lua
@@ -0,0 +1,70 @@
+local collisionModule = require("collision")
+
+local player = {
+  x = 100,
+  y = 350,
+  width = 40,
+  height = 40,
+  speed = 10,
+  velocity = {x = 0, y = 0},
+}
+
+local coll = nil
+
+function player.updatePlayer(dt)
+  movePlayer(dt)
+end
+
+function movePlayer(dt)
+  local oldPlayerX = player.x
+  local oldPlayerY = player.y
+  
+  -- move horizontally
+  if(love.keyboard.isDown("a") )then
+    player.velocity.x = player.velocity.x - player.speed * dt
+  elseif(love.keyboard.isDown("d") )then
+    player.velocity.x = player.velocity.x + player.speed * dt
+  end
+  
+  -- set velocity before to revert position based on collision
+  player.x = player.x + player.velocity.x
+  
+  -- check horitontal collision
+  coll = collisionModule.collisionAABB(player)
+  if coll then
+    player.velocity.x = 0
+    player.x = oldPlayerX -- revert x position only
+  end
+  
+  -- move vertically
+  if(love.keyboard.isDown("w") )then
+    player.velocity.y = player.velocity.y - player.speed * dt
+  elseif(love.keyboard.isDown("s") )then
+    player.velocity.y = player.velocity.y + player.speed * dt
+  end
+  
+  -- set velocity before to revert position based on collision
+  player.y = player.y + player.velocity.y
+  
+  -- check vertical collision
+  coll = collisionModule.collisionAABB(player)
+  if coll then
+    player.velocity.y = 0
+    player.y = oldPlayerY -- revert y position only
+  end
+  
+  
+  
+  
+end
+
+function player.drawPlayer()
+  love.graphics.rectangle("line", player.x, player.y, player.width, player.height)
+  
+  love.graphics.print("coll: " .. tostring(coll), 20, 20)
+  
+  love.graphics.print("velX: " .. player.velocity.x .. " velY: " .. player.velocity.y, 50, 50)
+end
+
+
+return player
