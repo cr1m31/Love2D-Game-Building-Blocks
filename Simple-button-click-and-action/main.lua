af3a60bc00c2320abc325e4db3d1ccef39129ced
cr1m31 <cr1m3man@gmail.com>
2025-08-07 20:33:06 +0200

    added a simple button list with click actions

---
+++ Simple-button-click-and-action/main.lua
@@ -0,0 +1,9 @@
+local buttonsModule = require("buttons")
+
+function love.draw()
+    buttonsModule.drawButtons()
+end
+
+function love.mousepressed(x, y, button, istouch)
+    buttonsModule.mousepressed(x, y, button, istouch)
+end
