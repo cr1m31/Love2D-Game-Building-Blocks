3c035e260606c7552dcd4013036c9ca0e6e2cc69
cr1m31 <cr1m3man@gmail.com>
2025-09-26 19:37:16 +0200

    first physics version yt tutorial

--- Physics-tests/player.lua
+++ Physics-tests/player.lua
@@ -8,11 +8,21 @@ local player = {
   width = 20,
   height = 40,
   speed = 200,
+  velocity = {x = 0, y = 0},
+  acceleration = 150,
+  gravity = {x = 0, y = 9.81},
 }
 
 function playerModule.update(dt)
   movePlayer(dt)
 end
+
+-- youtube tutorial about physics
+-- https://www.youtube.com/watch?v=5OG_Sw6hM84
+-- Formula 
+-- position += velocity * deltatime
+-- velocity += acceleration * deltatime
+-- velocity += gravity * deltatime
 
 function movePlayer(dt)
   local oldPlayerX = player.x
@@ -20,11 +30,11 @@ function movePlayer(dt)
   
   -- left
   if love.keyboard.isDown("a") then
-    player.x = player.x - player.speed * dt
+    player.velocity.x = player.velocity.x - player.acceleration * dt
   end
   -- right
   if love.keyboard.isDown("d") then
-    player.x = player.x + player.speed * dt
+    player.velocity.x = player.velocity.x + player.acceleration * dt
   end
   
   if gridModule.checkCollisionsBetweenPlayerAndTiles(player) then
@@ -33,16 +43,22 @@ function movePlayer(dt)
   
   -- up
   if love.keyboard.isDown("w") then
-    player.y = player.y - player.speed * dt
+    player.velocity.y = player.velocity.y - player.acceleration * dt
   end
   -- down
   if love.keyboard.isDown("s") then
-    player.y = player.y + player.speed * dt
+    player.velocity.y = player.velocity.y + player.acceleration * dt
   end
   
   if gridModule.checkCollisionsBetweenPlayerAndTiles(player) then
     player.y = oldPlayerY
   end
+  
+  player.x = player.x + player.velocity.x * dt
+  player.y = player.y + player.velocity.y * dt
+  player.velocity.y = player.velocity.y + player.gravity.y * dt
+  
+  
   
 end
 
@@ -55,6 +71,8 @@ function playerModule.draw()
 
   
   love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+  
+  love.graphics.print("velx: " .. player.velocity.x .. " vely: " .. player.velocity.y, 100, 200)
 end
 
 return playerModule
