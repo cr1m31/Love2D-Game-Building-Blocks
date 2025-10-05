127f78e66f0253602a70173a79553cf1787a0d90
cr1m31 <cr1m3man@gmail.com>
2025-10-05 22:40:43 +0200

    prepared scripts for vid on diagonal speed fix both with vector or not

--- Physics-tests/main.lua
+++ Physics-tests/main.lua
@@ -1,7 +1,7 @@
 io.stdout:setvbuf("no")
 
 local gridModule = require("grid")
-local playerModule = require("player-v3")
+local playerModule = require("player-no-vec-limit-diag-speed")
 
 function love.update(dt)
   playerModule.update(dt)
