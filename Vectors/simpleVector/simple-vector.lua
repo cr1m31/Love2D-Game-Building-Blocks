b27ad52aa3e8a6bc9b1d1fe0ac57793703f8a09a
cr1m31 <cr1m3man@gmail.com>
2025-10-24 00:00:17 +0200

    simplifyed the code and added strategic comments

--- Vectors/simpleVector/simple-vector.lua
+++ Vectors/simpleVector/simple-vector.lua
@@ -8,17 +8,25 @@ local centerY = screenHeight * 0.5
 
 local origin = {x = 0, y = 0}
 
-local vector = {0, 0}
+local vector = {x = 0, y = 0}
 
 local speed = 200
 local vectorTipRadius = 6
 
-function placeVectorEndPoint(vecX, vecY)
+local function matrixMultiplyVector(m, v)
+    -- multiply each horizontal row of the matrix by the column vector (vertical) to get new x and y
+    local x = m[1] * v.x + m[2] * v.y
+    local y = m[3] * v.x + m[4] * v.y
+    -- store results first so v.x isn't overwritten before computing v.y
+    v.x, v.y = x, y
+end
+
+local function placeVectorEndPoint(vecX, vecY)
   vector.x = vecX
   vector.y = vecY
 end
 
-function moveX(dt)
+local function moveX(dt)
   if love.keyboard.isDown("d") then
     vector.x = vector.x + speed * dt
   elseif love.keyboard.isDown("a") then
@@ -33,42 +41,35 @@ function moveX(dt)
 end
 
 local xProjectionMatrix = {
-    {1,0},
-    {0,0},
-  }
-function projectOnXaxis()
-  vector.x = (xProjectionMatrix[1][1] * vector.x) + (xProjectionMatrix[1][2] * vector.y)
-  vector.y = (xProjectionMatrix[2][1] * vector.x) + (xProjectionMatrix[2][2] * vector.y)
+    1,0,
+    0,0
+}
+local function projectOnXaxis()
+  matrixMultiplyVector(xProjectionMatrix, vector)
 end
 
 local xInversionMatrix = {
-    {1, 0},
-    {0,-1},
+    1, 0,
+    0,-1
 }
-function invertXaxis()
-    local vx, vy = vector.x, vector.y
-    vector.x = (xInversionMatrix[1][1] * vx) + (xInversionMatrix[1][2] * vy) 
-    vector.y = (xInversionMatrix[2][1] * vx) + (xInversionMatrix[2][2] * vy) 
+local function invertXaxis()
+    matrixMultiplyVector(xInversionMatrix, vector)
 end
 
 local yProjectionMatrix = {
-    {0,0},
-    {0,1},
-  }
-function projectOnYAxis()
-  vector.x = (yProjectionMatrix[1][1] * vector.x) + (yProjectionMatrix[1][2] * vector.y)
-  vector.y = (yProjectionMatrix[2][1] * vector.x) + (yProjectionMatrix[2][2] * vector.y)
+    0,0,
+    0,1
+}
+local function projectOnYAxis()
+  matrixMultiplyVector(yProjectionMatrix, vector)
 end
 
 local yInversionMatrix = {
-  {-1, 0},
-  { 0, 1},
+  -1, 0,
+   0, 1
 }
-
-function invertYaxis()
-  local vx, vy = vector.x, vector.y
-    vector.x = (yInversionMatrix[1][1] * vx) + (yInversionMatrix[1][2] * vy) 
-    vector.y = (yInversionMatrix[2][1] * vx) + (yInversionMatrix[2][2] * vy) 
+local function invertYaxis()
+  matrixMultiplyVector(yInversionMatrix, vector)
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -88,7 +89,6 @@ end
 
 function love.load()
   placeVectorEndPoint(30, 30)
-  
   simpleVectorAxisModule.createAxis(screenWidth,screenHeight)
 end
 
@@ -97,16 +97,14 @@ function love.update(dt)
 end
 
 function love.draw()
-  
   simpleVectorAxisModule.drawAxis()
-  
+
   love.graphics.line(origin.x + centerX, origin.y + centerY, vector.x + centerX, vector.y + centerY)
-  
   love.graphics.circle("line", vector.x + centerX, vector.y + centerY, vectorTipRadius)
   
-  love.graphics.print("vecX : " .. 
-    math.floor(vector.x - screenWidth * 0.5) + centerX .. 
-    " \nvecY : " .. 
-    math.floor(vector.y - screenHeight * 0.5) + centerY, 
+  love.graphics.print("vecX : " ..
+    math.floor(vector.x - screenWidth * 0.5) + centerX ..
+    " \nvecY : " ..
+    math.floor(vector.y - screenHeight * 0.5) + centerY,
     screenWidth * 0.2, screenHeight * 0.2)
 end
