3afc58471b7b0abd8ffd149405f0ad882da980c4
cr1m31 <cr1m3man@gmail.com>
2025-10-23 23:16:53 +0200

    fixed x and y axis matrices

--- Vectors/simpleVector/simple-vector.lua
+++ Vectors/simpleVector/simple-vector.lua
@@ -1,13 +1,12 @@
 local simpleVectorAxisModule = require("simpleVector.simple-vector-axis")
 
-
 local screenWidth = love.graphics.getWidth()
 local screenHeight = love.graphics.getHeight()
 
 local centerX = screenWidth * 0.5
 local centerY = screenHeight * 0.5
 
-local origin = {0 + centerX, 0 + centerY}
+local origin = {x = 0, y = 0}
 
 local vector = {0, 0}
 
@@ -15,8 +14,8 @@ local speed = 200
 local vectorTipRadius = 6
 
 function placeVectorEndPoint(vecX, vecY)
-  vector.x = vecX + centerX
-  vector.y = vecY + centerY
+  vector.x = vecX
+  vector.y = vecY
 end
 
 function moveX(dt)
@@ -39,16 +38,16 @@ local xProjectionMatrix = {
   }
 function projectOnXaxis()
   vector.x = (xProjectionMatrix[1][1] * vector.x) + (xProjectionMatrix[1][2] * vector.y)
-  vector.y = (xProjectionMatrix[2][1] * vector.x) + (xProjectionMatrix[2][2] * vector.y) + centerY
+  vector.y = (xProjectionMatrix[2][1] * vector.x) + (xProjectionMatrix[2][2] * vector.y)
 end
 
 local xInversionMatrix = {
-    {-1, 0},
-    { 0, 1}
+    {1, 0},
+    {0,-1},
 }
 function invertXaxis()
     local vx, vy = vector.x, vector.y
-    vector.x = (xInversionMatrix[1][1] * vx + centerX) + (xInversionMatrix[1][2] * vy + centerX) 
+    vector.x = (xInversionMatrix[1][1] * vx) + (xInversionMatrix[1][2] * vy) 
     vector.y = (xInversionMatrix[2][1] * vx) + (xInversionMatrix[2][2] * vy) 
 end
 
@@ -57,8 +56,19 @@ local yProjectionMatrix = {
     {0,1},
   }
 function projectOnYAxis()
-  vector.x = (yProjectionMatrix[1][1] * vector.x) + (yProjectionMatrix[1][2] * vector.y) + centerX
+  vector.x = (yProjectionMatrix[1][1] * vector.x) + (yProjectionMatrix[1][2] * vector.y)
   vector.y = (yProjectionMatrix[2][1] * vector.x) + (yProjectionMatrix[2][2] * vector.y)
+end
+
+local yInversionMatrix = {
+  {-1, 0},
+  { 0, 1},
+}
+
+function invertYaxis()
+  local vx, vy = vector.x, vector.y
+    vector.x = (yInversionMatrix[1][1] * vx) + (yInversionMatrix[1][2] * vy) 
+    vector.y = (yInversionMatrix[2][1] * vx) + (yInversionMatrix[2][2] * vy) 
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -70,6 +80,9 @@ function love.keypressed(key, scancode, isrepeat)
   end
   if key == "i" then 
     invertXaxis()
+  end
+  if key == "j" then
+    invertYaxis()
   end
 end
 
@@ -87,13 +100,13 @@ function love.draw()
   
   simpleVectorAxisModule.drawAxis()
   
-  love.graphics.line(origin[1], origin[2], vector.x, vector.y)
+  love.graphics.line(origin.x + centerX, origin.y + centerY, vector.x + centerX, vector.y + centerY)
   
-  love.graphics.circle("line", vector.x, vector.y, vectorTipRadius)
+  love.graphics.circle("line", vector.x + centerX, vector.y + centerY, vectorTipRadius)
   
   love.graphics.print("vecX : " .. 
-    math.floor(vector.x - screenWidth * 0.5) .. 
+    math.floor(vector.x - screenWidth * 0.5) + centerX .. 
     " \nvecY : " .. 
-    math.floor(vector.y - screenHeight * 0.5), 
+    math.floor(vector.y - screenHeight * 0.5) + centerY, 
     screenWidth * 0.2, screenHeight * 0.2)
 end
