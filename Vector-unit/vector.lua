513593c6f19bbc3fda0cdd77cf88d4041611b176
Claude Rime <cr1m3man@gmail.com>
2026-02-14 23:45:36 +0100

    trying to fix and simplify vector unit without updating in a sorta load function

--- Vector-unit/vector.lua
+++ Vector-unit/vector.lua
@@ -1,6 +1,6 @@
 local vectorModule = {}
 
-local vector = {x = 50, y = - 150, radius = 8}
+local vector = {x = 50, y = - 80}
 local origin = {x = love.graphics.getWidth() / 2, y = love.graphics.getHeight() / 2}
 
 local vectorFakeScaling = 120
@@ -9,26 +9,22 @@ function normalizeVector()
   local squaredMagnitude = vector.x * vector.x + vector.y * vector.y
   
   local magnitude = math.sqrt(squaredMagnitude)
-  
-  print(magnitude)
-  
+
   if magnitude == 0 then
     return
   end
   
-  vector.x = vector.x / magnitude * vectorFakeScaling
-  vector.y = vector.y / magnitude * vectorFakeScaling
+  vector.x = vector.x / magnitude
+  vector.y = vector.y / magnitude
   
-  magnitude = magnitude / magnitude * vectorFakeScaling
   return magnitude
 end
-normalizeVector()
 
 function vectorModule.draw()
-  love.graphics.line(origin.x, origin.y, vector.x + origin.x, vector.y + origin.y)
-  love.graphics.circle("fill", vector.x + origin.x, vector.y + origin.y, vector.radius)
+  love.graphics.line(origin.x, origin.y, vector.x * vectorFakeScaling + origin.x ,vector.y * vectorFakeScaling + origin.y)
+  love.graphics.circle("fill", vector.x * vectorFakeScaling + origin.x, vector.y * vectorFakeScaling + origin.y, 8)
   
-  love.graphics.print("x : " .. vector.x / normalizeVector() .. "\n y : " .. vector.y / normalizeVector() .. "\n magnitude : " .. normalizeVector() / vectorFakeScaling, 100, 100)
+  love.graphics.print("x : " .. vector.x .. "\n y : " .. vector.y .. "\n magnitude : " .. normalizeVector(), 100, 100)
 end
 
 return vectorModule
