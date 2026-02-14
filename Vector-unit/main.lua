624048863510d5d7ed87ecd05c8d388e939a9b0b
Claude Rime <cr1m3man@gmail.com>
2026-02-14 23:29:01 +0100

    simplifying vector creation and normalization not in a loop for now

--- Vector-unit/main.lua
+++ Vector-unit/main.lua
@@ -2,10 +2,6 @@ io.stdout:setvbuf("no")
 
 local vecrorMod = require("vector")
 
-function love.update()
-  vecrorMod.update()
-end
-
 function love.draw()
   vecrorMod.draw()
 end
--- Vector-unit/vector.lua
+++ Vector-unit/vector.lua
@@ -1,27 +1,34 @@
 local vectorModule = {}
 
-local vector = {originX = love.graphics.getWidth() / 2, originY = love.graphics.getHeight() / 2, x = 100, y = 200, radius = 8}
+local vector = {x = 50, y = - 150, radius = 8}
+local origin = {x = love.graphics.getWidth() / 2, y = love.graphics.getHeight() / 2}
+
+local vectorFakeScaling = 120
 
 function normalizeVector()
   local squaredMagnitude = vector.x * vector.x + vector.y * vector.y
   
   local magnitude = math.sqrt(squaredMagnitude)
+  
   print(magnitude)
-  --vector.x = vector.x / magnitude
-  --vector.y = vector.y / magnitude
   
-end
-
-function vectorModule.update()
-  -- vector.x = love.mouse.getX()
-  -- vector.y = love.mouse.getY()
+  if magnitude == 0 then
+    return
+  end
   
-  normalizeVector()
+  vector.x = vector.x / magnitude * vectorFakeScaling
+  vector.y = vector.y / magnitude * vectorFakeScaling
+  
+  magnitude = magnitude / magnitude * vectorFakeScaling
+  return magnitude
 end
+normalizeVector()
 
 function vectorModule.draw()
-  love.graphics.line(vector.originX, vector.originY, vector.x, vector.y)
-  love.graphics.circle("fill", vector.x, vector.y, vector.radius)
+  love.graphics.line(origin.x, origin.y, vector.x + origin.x, vector.y + origin.y)
+  love.graphics.circle("fill", vector.x + origin.x, vector.y + origin.y, vector.radius)
+  
+  love.graphics.print("x : " .. vector.x / normalizeVector() .. "\n y : " .. vector.y / normalizeVector() .. "\n magnitude : " .. normalizeVector() / vectorFakeScaling, 100, 100)
 end
 
 return vectorModule
