ef1de5e6ea80608bb8b558d047e8a5d607a2dd9a
Claude Rime <cr1m3man@gmail.com>
2026-02-17 00:31:56 +0100

    added keypressed to normalize

--- Vector-unit/vector.lua
+++ Vector-unit/vector.lua
@@ -1,9 +1,9 @@
 local vectorModule = {}
 
-local vector = {x = 50, y = - 80}
+local vector = {x = 5, y = - 8}
 local origin = {x = love.graphics.getWidth() / 2, y = love.graphics.getHeight() / 2}
 
-local vectorFakeScaling = 120
+local vectorFakeScaling = 25
 
 function normalizeVector()
   local squaredMagnitude = vector.x * vector.x + vector.y * vector.y
@@ -17,14 +17,33 @@ function normalizeVector()
   vector.x = vector.x / magnitude
   vector.y = vector.y / magnitude
   
+  magnitude = magnitude / magnitude
+  
   return magnitude
 end
 
+local getMagnitude = 0
+
+-- key n to normalize vector and v to get it back as before
+function love.keypressed(key, scan, isrepeat)
+  if key == "n" then
+    getMagnitude = normalizeVector()
+  end
+  
+  if key == "v" then
+    vector.x = 5
+    vector.y = - 8
+  end
+end
+
+  
+
+
 function vectorModule.draw()
   love.graphics.line(origin.x, origin.y, vector.x * vectorFakeScaling + origin.x ,vector.y * vectorFakeScaling + origin.y)
-  love.graphics.circle("fill", vector.x * vectorFakeScaling + origin.x, vector.y * vectorFakeScaling + origin.y, 8)
+  love.graphics.circle("line", vector.x * vectorFakeScaling + origin.x, vector.y * vectorFakeScaling + origin.y, 8)
   
-  love.graphics.print("x : " .. vector.x .. "\n y : " .. vector.y .. "\n magnitude : " .. normalizeVector(), 100, 100)
+  love.graphics.print("x : " .. vector.x .. "\n y : " .. vector.y .. "\n magnitude : " .. getMagnitude, 100, 100)
 end
 
 return vectorModule
