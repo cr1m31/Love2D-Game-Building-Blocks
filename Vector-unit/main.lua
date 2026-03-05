5471c21eac3c7c4e96e936009718d437189accb7
Claude Rime <cr1m3man@gmail.com>
2026-03-05 13:54:36 +0100

    ready for tutorial

--- Vector-unit/main.lua
+++ Vector-unit/main.lua
@@ -2,6 +2,11 @@ io.stdout:setvbuf("no")
 
 local vectorMod = require("vector")
 
+function love.load()
+  vectorMod.init()
+end
+
+
 function love.draw()
   vectorMod.draw()
 end
--- Vector-unit/vector.lua
+++ Vector-unit/vector.lua
@@ -1,23 +1,35 @@
 local vectorModule = {}
 
-local vector = {x = 4, y = - 3}
 local origin = {x = love.graphics.getWidth() / 2, y = love.graphics.getHeight() / 2}
+local vector = {x = 4, y = - 3}
+local unitVector = {x = 0, y = 0}
+
+local xOffset = 100
+local scaleVec = 50
 
 function normalizeVector()
   local magnitude = math.sqrt(vector.x * vector.x + vector.y * vector.y)
-  local unitVector = {x = vector.x / magnitude, y = vector.y / magnitude}
-  return magnitude, unitVector
+  unitVector.x = vector.x / magnitude
+  unitVector.y = vector.y / magnitude
+  local newMagnitude = magnitude / magnitude
+  return newMagnitude
 end
 
-local scaleFactor = 50
-local unitVecOffset = 100
-function vectorModule.draw()
-  local mag, unit = normalizeVector()
-  love.graphics.print("mag : " .. mag .. "\n" .. "vector.x : " .. vector.x .. "\n" .. "vector.y : " .. vector.y, 100, 100)
-  love.graphics.print("new mag : " .. (mag / mag) .. "\n" .. "unit.x : " .. unit.x .. "\n" .. "unit.y : " .. unit.y, 200, 100)
+local newMag = 0
+function vectorModule.init()
+  unitVector.x = vector.x
+  unitVector.y = vector.y
   
-  love.graphics.line(origin.x, origin.y, origin.x + vector.x * scaleFactor, origin.y + vector.y * scaleFactor)
-  love.graphics.line(origin.x + unitVecOffset, origin.y, origin.x + unitVecOffset + unit.x * scaleFactor, origin.y + unitVecOffset + unit.y * scaleFactor)
+  newMag = normalizeVector()
+  print(newMag)
+  print("unit vector x : " .. unitVector.x .. " unitVector.y : " .. unitVector.y)
+end
+
+function vectorModule.draw()
+  love.graphics.print("vector", origin.x, origin.y)
+  love.graphics.print("unitVector", origin.x + xOffset, origin.y)
+  love.graphics.line(origin.x, origin.y, origin.x + vector.x * scaleVec, origin.y + vector.y * scaleVec)
+  love.graphics.line(origin.x + xOffset, origin.y, origin.x + xOffset + unitVector.x * scaleVec, origin.y + unitVector.y * scaleVec)
 end
 
 return vectorModule
