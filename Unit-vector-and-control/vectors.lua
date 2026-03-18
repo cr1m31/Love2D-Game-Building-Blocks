4ae39335efaab5b8616bc3440637b5030f516f20
Claude Rime <cr1m3man@gmail.com>
2026-03-18 17:43:11 +0100

    trying to fix normalization

--- Unit-vector-and-control/vectors.lua
+++ Unit-vector-and-control/vectors.lua
@@ -1,10 +1,17 @@
 local vectorModule = {}
 
+local magnitude = 0
+
 function vectorModule.normalizeVector(player)
   local oldX = player.velocity.x
   local oldY = player.velocity.y
   
-  
+  magnitude = math.sqrt(player.velocity.x * player.velocity.x + player.velocity.y * player.velocity.y)
+  if magnitude == 0 then
+    return
+  end
+  player.velocity.x = player.velocity.x / magnitude
+  player.velocity.y = player.velocity.y / magnitude
 end
 
 function vectorModule.draw(player)
@@ -13,6 +20,8 @@ function vectorModule.draw(player)
     (player.y + player.height / 2) + player.velocity.y)
   
   love.graphics.circle("line", player.x + player.width / 2, player.y + player.height / 2, player.velocityLimit)
+  
+  love.graphics.print("mag: " .. math.floor(magnitude), 150, 150)
 end
 
 return vectorModule
