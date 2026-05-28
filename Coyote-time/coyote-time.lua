3b7ef1992e7920b647ed7bcbd9e97879bc053c12
Claude Rime <cr1m3man@gmail.com>
2026-05-28 22:55:35 +0200

    fixed vertical collision with gravity and acceleration but need to fix wall sticking

--- Coyote-time/coyote-time.lua
+++ Coyote-time/coyote-time.lua
@@ -1,11 +1,15 @@
 local coyoteTimeModule = {}
 
 local player = {
-  x = 100,
-  y = 200,
+  x = 200,
+  y = 100,
   width = 50,
   height = 50,
-  speed = 150,
+  speed = 10,
+  velocity = {x = 0, y = 0},
+  acceleration = 3,
+  gravity = 8.1,
+  jumpForce = 3,
 }
 
 local platform = {
@@ -21,25 +25,35 @@ function movePlayer(dt)
   local oldY = player.y
   
   if love.keyboard.isDown("a") then
-    player.x = player.x - player.speed * dt
+    player.velocity.x = player.velocity.x - player.speed * player.acceleration * dt
   end
   if love.keyboard.isDown("d") then
-    player.x = player.x + player.speed * dt
+    player.velocity.x = player.velocity.x + player.speed * player.acceleration * dt
   end
   
   if not collisionCheck(player, platform) then
+    
+    
     player.x = oldX
+    player.velocity.x = 0
   end
   
-  if love.keyboard.isDown("w") then
-    player.y = player.y - player.speed * dt
-  end
-  if love.keyboard.isDown("s") then
-    player.y = player.y + player.speed * dt
-  end
   
+  -- ALSO NEED TO FIND WHY WALL STICKING HAPPENS WHEN PLAYER TOUCHES SIDES ???
+  
+  
+  player.x = player.x + player.velocity.x
+  player.y = player.y + player.velocity.y
+  
+  -- NEED TO FIND WHY COLLISION IS WORKING (NO GROUND STICKING) WHEN MOVING THIS CODE AFTER PLAYER POSITION UPDATE ???
   if not collisionCheck(player, platform) then
+    
     player.y = oldY
+    player.velocity.y = 0
+  
+  else
+  
+    
   end
   
 end
@@ -51,9 +65,14 @@ function collisionCheck(aa, bb)
     aa.y > bb.y + bb.height
 end
 
+function addGravity(dt)
+  player.velocity.y = player.velocity.y + player.gravity * dt
+end
+
 
 function coyoteTimeModule.update(dt)
   movePlayer(dt)
+  addGravity(dt)
 end
 
 
