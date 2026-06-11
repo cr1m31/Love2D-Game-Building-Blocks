9e3aa1528d6e4feeb661b953370d7adc733c5885
Claude Rime <cr1m3man@gmail.com>
2026-06-11 17:38:36 +0200

    removed player speed and let only acceleration added or removed from velocity

--- Coyote-time/coyote-time.lua
+++ Coyote-time/coyote-time.lua
@@ -7,9 +7,8 @@ local player = {
   y = 100,
   width = 50,
   height = 50,
-  speed = 150,
   velocity = {x = 0, y = 0},
-  horizontalAcceleration = 6,
+  horizontalAcceleration = 600,
   jumpForce = 3,
 }
 
@@ -25,10 +24,10 @@ function movePlayer(dt)
   local oldY = player.y
   
   if love.keyboard.isDown("a") then
-    player.velocity.x = player.velocity.x - player.speed * player.horizontalAcceleration * dt
+    player.velocity.x = player.velocity.x - player.horizontalAcceleration * dt
   end
   if love.keyboard.isDown("d") then
-    player.velocity.x = player.velocity.x + player.speed * player.horizontalAcceleration * dt
+    player.velocity.x = player.velocity.x + player.horizontalAcceleration * dt
   end
   
   -- NEED TO CHANGE PLAYER POSITION BEFORE COLLISION CHECK !! (TO PREVENT WALL STICKING)
