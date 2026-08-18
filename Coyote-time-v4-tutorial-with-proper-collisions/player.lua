53b826245825bcb8e92fa11b88a1658106d4e8b0
Claude Rime <cr1m3man@gmail.com>
2026-08-18 20:05:54 +0200

    added player horizontal movement in new code from scratch

--- Coyote-time-v4-tutorial-with-proper-collisions/player.lua
+++ Coyote-time-v4-tutorial-with-proper-collisions/player.lua
@@ -5,10 +5,25 @@ local player = {
   y = 100,
   width = 30,
   height = 50,
+  speed = 100,
 }
 
-function playerModule.update(dt)
+function movePlayer(dt)
+  if love.keyboard.isDown("a") then
+    player.x = player.x - player.speed * dt
+  end
+  
+  if love.keyboard.isDown("d") then
+    player.x = player.x + player.speed * dt
+  end
+  
+  
+  
+end
 
+
+function playerModule.update(dt)
+  movePlayer(dt)
 end
 
 function playerModule.draw()
