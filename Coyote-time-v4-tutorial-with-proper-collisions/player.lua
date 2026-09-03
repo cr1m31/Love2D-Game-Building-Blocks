2458989311842c8042b64bde495cadc327876daf
Claude Rime <cr1m3man@gmail.com>
2026-09-03 18:55:33 +0200

    added tiles and implemented non effective collisions especially for jumping under a ceiling with higher jump thant its under height

--- Coyote-time-v4-tutorial-with-proper-collisions/player.lua
+++ Coyote-time-v4-tutorial-with-proper-collisions/player.lua
@@ -9,32 +9,28 @@ local player = {
   velocity = {x = 0, y = 0},
 }
 
-function movePlayer(dt)
-  if love.keyboard.isDown("a") then
-    player.x = player.x - player.speed * dt
-  end
-  
-  if love.keyboard.isDown("d") then
-    player.x = player.x + player.speed * dt
-  end
-  
-  
-  
-  player.y = player.y + player.velocity.y * dt
-  
-  addGravity()
-  
-end
+local platform = {
+  x = 100,
+  y = 400,
+  width  = 700,
+  height = 20,
+}
 
-function playerJump()
-  print("JUMP 0: " .. player.velocity.y)
-  player.velocity.y = - 2000
-  print("JUMP : " .. player.velocity.y)
-end
+local wall = {
+  x = 0,
+  y = 200,
+  width  = 20,
+  height = 250,
+}
 
-function addGravity()
-  player.velocity.y = 50
-end
+local ceiling = {
+  x = 150,
+  y = 300,
+  width  = 600,
+  height = 20,
+}
+
+local tiles = {}
 
 function collisionCheck(aa, bb)
   return aa.x + aa.width > bb.x and
@@ -43,12 +39,73 @@ function collisionCheck(aa, bb)
     aa.y < bb.y + bb.height
 end
 
+function movePlayer(dt)
+  local oldX = player.x
+  local oldY = player.y
+  
+  if love.keyboard.isDown("a") then
+    player.x = player.x - player.speed * dt
+  end
+  
+  if love.keyboard.isDown("d") then
+    player.x = player.x + player.speed * dt
+  end
+  
+  for _, tile in ipairs(tiles) do
+    if collisionCheck(player, tile) then
+      player.x = oldX
+    end
+  end
+  
+  
+  player.y = player.y + player.velocity.y * dt
+  
+  addGravity()
+  
+  for _, tile in ipairs(tiles) do
+    if collisionCheck(player, tile) then
+      
+      if player.velocity.y < 0 then
+        player.y = tile.y - player.height
+      elseif player.velocity.y >= 0 then
+        player.y = oldY
+      
+      end
+      
+    end
+  end
+  
+end
+
+function playerJump()
+  print("JUMP 0: " .. player.velocity.y)
+  player.velocity.y = - 1700
+  print("JUMP : " .. player.velocity.y)
+end
+
+function addGravity()
+  player.velocity.y = 50
+end
+
+
+function playerModule.load()
+  table.insert(tiles, 1, platform)
+  table.insert(tiles, 2, wall)
+  table.insert(tiles, 3, ceiling)
+end
+
 
 function playerModule.update(dt)
   movePlayer(dt)
 end
 
 function playerModule.draw()
+  
+  for _, tile in ipairs(tiles) do
+    love.graphics.rectangle("line", tile.x, tile.y, tile.width, tile.height)
+  end
+  
+  
   love.graphics.rectangle("line", player.x, player.y, player.width, player.height)
   
   love.graphics.print("vel y : " .. player.velocity.y, 300, 300)
